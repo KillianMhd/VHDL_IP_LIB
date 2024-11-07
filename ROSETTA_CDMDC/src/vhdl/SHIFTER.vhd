3c79c3c78d730f0531c3c48d401e1c329967b508
KillianMhd <killianmichaud1@gmail.com>
2024-11-07 16:20:16 +0100

    -new project ROSETTA_CDMDC

---
+++ ROSETTA_CDMDC/src/vhdl/SHIFTER.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07.11.2024 16:04:00
+-- Design Name: 
+-- Module Name: SHIFTER - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity SHIFTER is
+--  Port ( );
+end SHIFTER;
+
+architecture Behavioral of SHIFTER is
+
+begin
+
+
+end Behavioral;
---
+++ ROSETTA_CDMDC/src/vhdl/WB_INTERFACE.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07.11.2024 16:06:58
+-- Design Name: 
+-- Module Name: WB_INTERFACE - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity WB_INTERFACE is
+--  Port ( );
+end WB_INTERFACE;
+
+architecture Behavioral of WB_INTERFACE is
+
+begin
+
+
+end Behavioral;
