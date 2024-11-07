9f127b6cf21f7e715ce1e399defdd546703ce6d8
KillianMhd <killianmichaud1@gmail.com>
2024-11-07 17:12:10 +0100

    -add verilog sources

---
+++ ROSETTA_CDMDC/src/verilog/SECTOR_DECODER.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.11.2024 17:10:47
+// Design Name: 
+// Module Name: SECTOR_DECODER
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SECTOR_DECODER(
+
+    );
+endmodule
